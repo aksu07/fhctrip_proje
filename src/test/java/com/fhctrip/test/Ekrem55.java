ef41d2aa0c8abf5ae5309c883a1105417aed7abd
Ekram <ekremkose2001@gmail.com>
2020-08-26 23:39:22 -0500

    Second commit local repo

--- src/test/java/com/fhctrip/test/Ekrem55.java
+++ src/test/java/com/fhctrip/test/Ekrem55.java
@@ -3,6 +3,7 @@ package com.fhctrip.test;
 public class Ekrem55 {
     public static void main(String[] args) {
         System.out.println("New a commit lokal repository");
+        System.out.println("Second commit from ekrem repo");
     }
 
 }
